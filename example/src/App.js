c250b1e0c8b2cd7df68fc168df6e8e024d2ab6cd
Cafer Can Gündoğdu <cafercangundogdu@gmail.com>
2022-09-22 23:58:41 +0300

    update version

--- example/src/App.js
+++ example/src/App.js
@@ -219,7 +219,7 @@ const App = ({ theme, setPalette }) => {
               variant='h6'
               color='secondary'
             >
-              v2.3.8 <span role='img'>🥳</span>
+              v2.4.0 <span role='img'>🥳</span>
             </Typography>
           </Typography>
           <Tooltip title='Toggle Dark/Light Mode'>
